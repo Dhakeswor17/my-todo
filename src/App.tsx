5627c6b9b631451258cc4289cbbb0be24215b66b
Dhakeswor Nyaupane <dhakeswn@metropolia.fi>
2025-02-16 12:50:18 +0200

    makng router dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,31 @@
-import React from 'react'
-import Home from './components/Home'
+
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Home from './pages/Home'
+import NotFound from './pages/NotFound'
+import Register from './pages/Register'
 
 const App = () => {
+
+  const router = createBrowserRouter([
+
+    {
+      path: '/',
+      element: <Home></Home>
+    },
+    {
+      path:'Register',
+      element: <Register></Register>,
+      errorElement: <NotFound></NotFound>
+      
+    }
+  ])
   return (
     <div>
-      <Home></Home>
-    </div>
+    
+  <RouterProvider router={router}></RouterProvider>
+  </div>
   )
+  
 }
 
 export default App
