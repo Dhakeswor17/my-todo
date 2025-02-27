470464a0843b869b54176ebc3a25051e00adb507
dhakeswor <santoshneupane17@gmail.com>
2025-02-27 23:36:34 +0200

    making the home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,22 +1,15 @@
-import NavigationIcon from '@mui/icons-material/Navigation'
 import { Link } from 'react-router-dom'
-import Fab from '@mui/material/Fab'
-import { Box } from '@mui/material'
 
 const Home = () => {
   return (
-    <Box>
-      <Fab 
-        component={Link}  // ✅ Makes Fab act like a Link
-        to="/register" 
-        variant="extended"
-      >
-        Register
-        <NavigationIcon sx={{ mr: 1 }} />
-      </Fab>
-    </Box>
+    <div>
+      <header>
+        <Link to='/Register'>Register</Link>
+      </header>
+      <h1>Welcome to Todo App</h1>
+      <Link to='/Todo'>Go to Todos</Link>
+    </div>
   )
 }
 
 export default Home
-
--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,7 +1,7 @@
 
 const NotFound = () => {
   return (
-    <div>There is some problem </div>
+    <div>There is some problem, Please try again or Make sure you enter right address </div>
   )
 }
 
